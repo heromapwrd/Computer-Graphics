032fc5802c15b4a28daed75fe764eb406f27705e
liuquanbo <heromapwrd@foxmail.com  [credential]    helper = store>
2016-06-27 23:12:04 +0800

    Implement Vector3

--- Vector3/Vector3/Vector3.cpp
+++ Vector3/Vector3/Vector3.cpp
@@ -3,15 +3,107 @@
 //					Vector3.h
 //
 ////////////////////////////////////////////////////////////
-#ifndef _VECTOR3_H__
-#define _VECTOR#_H__
-class Vector3
+#include "Vector3.h"
+#include<math.h>
+Vector3::Vector3()
 {
-public:
-protected:
-private:
-};
 
+}
 
+Vector3::Vector3(const Vector3& v) :x(v.x), y(v.y), z(v.z)
+{
 
-#endif
+}
+
+Vector3::Vector3(float fx, float fy, float fz) : x(fx), y(fy), z(fz)
+{
+
+}
+
+Vector3& Vector3::operator =(const Vector3& v)
+{
+	x = v.x;
+	y = v.y;
+	z = v.z; 
+	return *this;
+}
+
+bool Vector3::operator == (const Vector3& v)const
+{
+	return (x == v.x) && (y == v.y) && (z == v.z);
+}
+
+bool Vector3::operator != (const Vector3& v)const
+{
+	return (x != v.x) || (y != v.y) || (z != v.z);
+}
+
+Vector3 Vector3::operator +(const Vector3& v)const
+{
+	return Vector3(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3 Vector3::operator -(const Vector3& v)const
+{
+	return Vector3(x - v.x, y - v.y, z - v.z);
+}
+
+Vector3 Vector3::operator *(float a)const
+{
+	return Vector3(x*a, y*a, z*a);
+}
+
+Vector3 Vector3::operator /(float a)const
+{
+	float oneOverA = 1 / a;
+	return Vector3(x*oneOverA, y*oneOverA, z*oneOverA);
+}
+
+Vector3& Vector3::operator +=(const Vector3& v)
+{
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	return *this;
+}
+
+Vector3& Vector3::operator -=(const Vector3& v)
+{
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+	return *this;
+}
+
+Vector3& Vector3::operator *=(float a)
+{
+	x *= a;
+	y *= a;
+	z *= a;
+	return *this;
+}
+
+Vector3& Vector3::operator /=(float a)
+{
+	x /= a;
+	y /= a;
+	z /= a;
+	return *this;
+}
+
+void Vector3::normalize()
+{
+	float magSq = x*x + y*y + z*z;
+	if (magSq > 0.0f)
+	{
+		float oneOverMag = 1.0f / sqrt(magSq);
+		x *= oneOverMag;
+		y *= oneOverMag;
+		z *= oneOverMag;
+	}
+}
+
+float Vector3::operator * (const Vector3& v)const
+{
+	return x*v.x + y*v.y + z*v.z;
+}
